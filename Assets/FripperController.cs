fcc9bae6737139507e2ad09b38d9a0b4d44f2e43
tango2501 <79126030+tango2501@users.noreply.github.com>
2021-03-04 10:28:37 +0900

    L5C5.6完了時
    フリッパーを動かすスクリプトを作成完了時 保存

---
+++ Assets/FripperController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+    //HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20f;
+    //弾いたときの傾き
+    private float frickAngle = -20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+        //上記二つでフリッパーの初期位置を設定している
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左矢印キーを押したとき左フリッパーを動かす
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.frickAngle);
+        }
+        //右矢印キーを押したとき右フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.frickAngle);
+        }
+
+        //矢印キーが離された時にフリッパーを元に戻す
+        if(Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(defaultAngle);
+        }
+        if(Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(defaultAngle);
+        }
+    }
+    //フリッパーの傾きを設定
+    public void SetAngle (float angle)
+    {
+        JointSpring JointSpr = this.myHingeJoint.spring;
+        JointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = JointSpr;
+    }
+}
